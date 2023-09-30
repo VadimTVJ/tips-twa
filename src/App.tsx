9eb65200c23903dc9385378ab1655c0fd0e1c2c8
vadim.tvj <vadim.tvj@gmail.com>
2023-09-30 18:28:05 +0300

    chore(DX): husky added

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,10 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button
+          type="button"
+          onClick={() => setCount((prev) => prev + 1)}
+        >
           count is
           {' '}
           {count}
